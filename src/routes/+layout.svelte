38ea141c115e93fc8be1cb557a00fad45c2e88ee
Jarne Verstraete <jarne.verstraete@gmail.com>
2025-04-27 11:35:19 +0200

    v1 vibeboard with PWA

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+
+
+<script lang="ts">
+	import '../app.css';
+	
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="manifest" href="/manifest.json">
+	<meta name="theme-color" content="#2563eb">
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+	<link rel="apple-touch-icon" href="/icon-192.png" />
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    function playSound(filename: string) {
+        const audio = new Audio(`/sounds/${filename}.mp3`);
+        audio.play();
+    }
+</script>
+
+<div class="min-h-screen flex flex-col items-center justify-center bg-gray-200">
+    <div class="w-full max-w-xs bg-white rounded-lg shadow-lg p-6">
+        <h1 class="text-3xl font-bold text-center mb-6">Vibeboard 😎</h1>
+
+        <div class="grid grid-cols-2 gap-4">
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('gehaald')}>gehaald</button>
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('plopperdeplop')}>Plopperdeplop</button>
+        </div>
+        <div class="grid grid-cols-2 gap-4">
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('hallowki')}>hallowki</button>
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('toenplots')}>toenplots</button>
+        </div>
+        <div class="grid grid-cols-2 gap-4">
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('luiheeftaids')}>luiheeftaids</button>
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('ikrookeenpijp')}>rookeenpijp</button>
+        </div>
+        <div class="grid grid-cols-2 gap-4">
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('eruit')}>eruit</button>
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('behalvelui')}>behalvelui</button>
+        </div>
+        <div class="grid grid-cols-2 gap-4">
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('dommerik')}>dommerik</button>
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('seksmetsmal')}>seksmetsmal</button>
+        </div>
+        <div class="grid grid-cols-2 gap-4">
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('klus!')}>klus!</button>
+            <button class="bg-red-500 text-white p-4 rounded-md mb-4 shadow-lg active:bg-red-700" onclick={() => playSound('geenaids')}>geenaids</button>
+        </div>
+    </div>
+</div>
